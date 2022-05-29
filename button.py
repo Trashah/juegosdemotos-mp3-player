3d57113b6f657d0bbd8fbf12db8df0e1a196e4de
Trashah <l215303023@outlook.com>
2022-05-29 16:30:50 -0500

    Update

--- button.py
+++ button.py
@@ -1,7 +1,9 @@
 import pygame
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
 
 class Button():
-    #constructor
     def _init_(self, x , y, image, scale):
         width = image.get_width()
         height = image.get_height()
@@ -29,26 +31,6 @@ class Button():
 
         return action
 
-pygame.init()
-
-#create game window
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("juegosdemotos mp3 player")
-
-#mp3 variaables
-mp3_paused = False
-menu_state = "main"
-
-#define fonts
-font = pygame.font.SysFont("arialblack", 40)
-
-#define colours
-TEXT_COL = (255, 255, 255)
-
-#load button images
 stop_img = pygame.image.load("images/stop.png").convert_alpha
 play_img = pygame.image.load("images/play.png").convert_alpha
 next_img = pygame.image.load("images/next.png").convert_alpha
@@ -57,17 +39,35 @@ quit_img = pygame.image.load("images/quit.png").convert_alpha
 previous_img = pygame.image.load("images/previous.png").convert_alpha
 settings_img = pygame.image.load("images/settings.png").convert_alpha
 
-stop_button = Button(304, 125, stop_img, 1)
-play_button = Button(297, 250, play_img, 1)
-next_button = Button(336, 375, next_img, 1)
-back_button = Button(226, 75, back_img, 1)
-quit_button = Button(225, 200, quit_img, 1)
-previous_button = Button(246, 325, previous_img, 1)
-settings_button = Button(332, 450, settings_img, 1)
+class MusicPlayer():
+    def _init_(self, win):
+        #create Tkinter window
+        win.geometry("200x200")
+        win.title("juegosdemotos mp3 player")
+        win.resizable(0, 0)
 
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img (x, y))
+        
+        stop_button = Button(304, 125, stop_img, 1)
+        play_button = Button(297, 250, play_img, 1)
+        next_button = Button(336, 375, next_img, 1)
+        back_button = Button(226, 75, back_img, 1)
+        quit_button = Button(225, 200, quit_img, 1)
+        previous_button = Button(246, 325, previous_img, 1)
+        settings_button = Button(332, 450, settings_img, 1)
 
-#game loop
-run = True
+    def load(self):
+        pass
+
+    def play(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def stop(self):
+        pass
+
+root = Tk()
+MusicPlayer(root)
+
+root.mainloop()
