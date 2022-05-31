4129dc25bfe856b94ee89d122f23daf094f418b5
Trashah <l215303023@outlook.com>
2022-05-31 02:56:04 -0500

    Functional volume buttons

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame import mixer
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog
+import os
 
 pygame.init()
 
@@ -53,6 +54,7 @@ class MusicPlayer:
         self.playing_state = False
         self.background_state = True
         self.paused = False
+        self.volume_change = 0.1
 
     def intro(self):
         mixer.init()
@@ -62,7 +64,10 @@ class MusicPlayer:
     def load(self):
             self.music_file = filedialog.askopenfilename()
             mixer.init()
-            if self.music_file: mixer.music.load(self.music_file)
+            if self.music_file: 
+                mixer.music.load(self.music_file)
+                self.music_name = (os.path.basename(self.music_file).split(".")[0])
+                pygame.display.set_caption("juegos de motos mp3: ", self.music_name)
             pass
 
     def play(self):
@@ -83,13 +88,19 @@ class MusicPlayer:
         pass
 
     def stop(self):
-        mixer.music.stop()
+        mixer.music.fadeout(3)
         self.music_file = None
         self.playing_state = False
         pass
 
-    def changeLayout(self):
-        pass
+    def volume_increase(self):
+        mixer.music.set_volume(mixer.music.get_volume() + self.volume_change)
+
+    def volume_decrease(self):
+        if mixer.music.get_volume() <= 0.1: 
+            mixer.music.set_volume(0)
+        else: 
+            mixer.music.set_volume(mixer.music.get_volume() - self.volume_change)
 
 #create images
 stop_img = pygame.image.load("images/stop.png").convert_alpha()
@@ -101,8 +112,11 @@ pause_img = pygame.image.load("images/pause.png").convert_alpha()
 open_img = pygame.image.load("images/open.png").convert_alpha()
 previous_img = pygame.image.load("images/previous.png").convert_alpha()
 settings_img = pygame.image.load("images/settings.png").convert_alpha()
+volumeplus_img = pygame.image.load("images/volume+.png").convert_alpha()
+volumeminus_img = pygame.image.load("images/volume-.png").convert_alpha()
 background1_img = pygame.image.load("images/background1.png").convert_alpha()
 background2_img = pygame.image.load("images/background2.png").convert_alpha()
+
 
 #create buttons
 stop_button = Button(611, 464, stop_img, 0.37)
@@ -114,6 +128,8 @@ exit_button = Button(661, 464, exit_img, .19)
 pause_button = Button(661, 389, pause_img, 0.18)
 previous_button = Button(246, 325, previous_img, 1)
 settings_button = Button(332, 450, settings_img, 1)
+volumeplus_button = Button(0, 0, volumeplus_img, 0.5)
+volumeminus_button = Button(300, 0, volumeminus_img, 0.5)
 
 #create background
 background1 = Background(0, 0, background1_img, 1)
@@ -150,6 +166,11 @@ while run:
     if exit_button.draw(screen):
         run = False
 
+    if volumeplus_button.draw(screen):
+        mp3.volume_increase()
+
+    if volumeminus_button.draw(screen):
+        mp3.volume_decrease()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
