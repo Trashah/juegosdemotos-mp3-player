da13674daa6cfd3dbb25300da65ff271448f5b40
Trashah <l215303023@outlook.com>
2022-05-30 22:24:38 -0500

    Commit

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("juegosdemotos mp3")
 
-menu_state = "main"
-
 class ImageHandler:
     def __init__(self, x , y, image, scale):
         width = image.get_width()
@@ -48,34 +46,6 @@ class Button(ImageHandler):
 
         return action
        
-#create images
-stop_img = pygame.image.load("images/stop.png").convert_alpha()
-play_img = pygame.image.load("images/play.png").convert_alpha()
-next_img = pygame.image.load("images/next.png").convert_alpha()
-back_img = pygame.image.load("images/back.png").convert_alpha()
-exit_img = pygame.image.load("images/exit.png").convert_alpha()
-pause_img = pygame.image.load("images/pause.png").convert_alpha()
-open_img = pygame.image.load("images/open.png").convert_alpha()
-previous_img = pygame.image.load("images/previous.png").convert_alpha()
-settings_img = pygame.image.load("images/settings.png").convert_alpha()
-background1_img = pygame.image.load("images/background1.png").convert_alpha()
-background2_img = pygame.image.load("images/background2.png").convert_alpha()
-
-#create buttons
-stop_button = Button(611, 464, stop_img, 0.37)
-open_button = Button(618, 278, open_img, 0.4)
-play_button = Button(611, 389, play_img, 0.18)
-next_button = Button(336, 375, next_img, 1)
-back_button = Button(226, 75, back_img, 1)
-exit_button = Button(661, 464, exit_img, .19)
-pause_button = Button(661, 389, pause_img, 0.18)
-previous_button = Button(246, 325, previous_img, 1)
-settings_button = Button(332, 450, settings_img, 1)
-
-#create background
-background1 = Background(0, 0, background1_img, 1)
-background2 = Background(10, 50, background2_img, 0.23)
-
 class MusicPlayer:
     def __init__(self):
         self.music_file = False
@@ -121,6 +91,36 @@ class MusicPlayer:
     def changeLayout(self):
         pass
 
+#create images
+stop_img = pygame.image.load("images/stop.png").convert_alpha()
+play_img = pygame.image.load("images/play.png").convert_alpha()
+next_img = pygame.image.load("images/next.png").convert_alpha()
+back_img = pygame.image.load("images/back.png").convert_alpha()
+exit_img = pygame.image.load("images/exit.png").convert_alpha()
+pause_img = pygame.image.load("images/pause.png").convert_alpha()
+open_img = pygame.image.load("images/open.png").convert_alpha()
+previous_img = pygame.image.load("images/previous.png").convert_alpha()
+settings_img = pygame.image.load("images/settings.png").convert_alpha()
+background1_img = pygame.image.load("images/background1.png").convert_alpha()
+background2_img = pygame.image.load("images/background2.png").convert_alpha()
+
+#create buttons
+stop_button = Button(611, 464, stop_img, 0.37)
+open_button = Button(618, 278, open_img, 0.4)
+play_button = Button(611, 389, play_img, 0.18)
+next_button = Button(336, 375, next_img, 1)
+back_button = Button(226, 75, back_img, 1)
+exit_button = Button(661, 464, exit_img, .19)
+pause_button = Button(661, 389, pause_img, 0.18)
+previous_button = Button(246, 325, previous_img, 1)
+settings_button = Button(332, 450, settings_img, 1)
+
+#create background
+background1 = Background(0, 0, background1_img, 1)
+background2 = Background(10, 50, background2_img, 0.23)
+
+
+
 mp3 = MusicPlayer()
 
 run = True
@@ -129,7 +129,7 @@ mp3.intro()
 while run:
     
     screen.fill((255, 255, 255))
-    if mp3.background_state:
+    if mp3.background_state: #Implementar cambio de fondos
         background1.draw(screen)
     else:
         background2.draw(screen)
